9255a5fb3a840317d4dbc63ec5a66a9f4024da5b
A-Minor15 <degtju15@outlook.jp>
2023-12-05 21:47:05 +0900

    added the test-file.php modified the Register.php

--- classes/Register.php
+++ classes/Register.php
@@ -43,6 +43,7 @@
                         <button type="submit" class="btn btn-success w-100">Register</button>
                     </form>
                     <p class="text-center mt-3 small">Registered Already?<a href="../views/index.php">Login</a></p>
+                    <p class="text-center">Kredo 2023</p>
                 </div>
             </div>
         </div>
